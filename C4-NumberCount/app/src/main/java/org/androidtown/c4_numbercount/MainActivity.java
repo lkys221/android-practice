cd4fefaf452ae203f458ba7ad512ad1fa5d7c58c
Leo .Kim <lkys221@snu.ac.kr>
2018-01-12 11:30:36 +0900

    xml complete

--- C4-NumberCount/app/src/main/java/org/androidtown/c4_numbercount/MainActivity.java
+++ C4-NumberCount/app/src/main/java/org/androidtown/c4_numbercount/MainActivity.java
@@ -2,12 +2,29 @@ package org.androidtown.c4_numbercount;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+    EditText msg;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        msg = (EditText) findViewById(R.id.textbox);
+
     }
+
+
+    public void onSendBtnClicked(View v){
+
+    }
+
+    public void onCloseBtnClicked(View v){
+
+    }
+
+
 }
