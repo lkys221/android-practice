459b52a9c90b43ecaf177d0bb78eb0b2f664ec63
Leo .Kim <lkys221@snu.ac.kr>
2018-01-27 13:59:20 +0900

    Main-Menu intent

--- C6-MultipleActivities/app/src/main/java/org/androidtown/myapplication/MainActivity.java
+++ C6-MultipleActivities/app/src/main/java/org/androidtown/myapplication/MainActivity.java
@@ -1,13 +1,35 @@
 package org.androidtown.myapplication;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+    final static int REQUEST_CODE_MENU = 101;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button loginBtn = (Button) findViewById(R.id.loginBtn);
+        final EditText username = (EditText) findViewById(R.id.username);
+        final EditText password = (EditText) findViewById(R.id.password);
+
+        loginBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, MenuActivity.class);
+                intent.putExtra("Username", String.valueOf(username.getText()));
+                intent.putExtra("Password", String.valueOf(password.getText()));
+                startActivityForResult(intent, REQUEST_CODE_MENU);
+            }
+        });
+
     }
+
+
 }
--- C6-MultipleActivities/app/src/main/java/org/androidtown/myapplication/MenuActivity.java
+++ C6-MultipleActivities/app/src/main/java/org/androidtown/myapplication/MenuActivity.java
@@ -1,7 +1,9 @@
 package org.androidtown.myapplication;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class MenuActivity extends AppCompatActivity {
 
@@ -9,5 +11,11 @@ public class MenuActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu);
+
+        Intent intent = new Intent(this.getIntent());
+        String username = intent.getStringExtra("Username");
+        String password = intent.getStringExtra("Password");
+
+        Toast.makeText(getApplicationContext(), "Username: "  + username + " Password: " + password, Toast.LENGTH_LONG).show();
     }
 }
