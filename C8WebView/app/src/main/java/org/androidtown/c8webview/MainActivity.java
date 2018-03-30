75b9f76ec1747066c24d421efe27bcdcc12bba48
Leo .Kim <lkys221@snu.ac.kr>
2018-03-30 20:26:45 +0900

    C8 complete

--- C8WebView/app/src/main/java/org/androidtown/c8webview/MainActivity.java
+++ C8WebView/app/src/main/java/org/androidtown/c8webview/MainActivity.java
@@ -2,12 +2,92 @@ package org.androidtown.c8webview;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.LinearLayout;
 
 public class MainActivity extends AppCompatActivity {
+    LinearLayout slide;
+    Animation translateBottom, translateClose;
+    Button button;
+    boolean isPageOpen = false;
+    WebView webView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        slide = (LinearLayout) findViewById(R.id.url);
+        button = (Button) findViewById(R.id.button);
+        translateBottom = AnimationUtils.loadAnimation(this, R.anim.translate_down);
+        translateClose = AnimationUtils.loadAnimation(this, R.anim.translate_close);
+
+        webView = (WebView) findViewById(R.id.webview);
+        WebSettings settings = webView.getSettings();
+        settings.setJavaScriptEnabled(true);
+        webView.setWebViewClient(new WebViewClient());
+        Button button2 = (Button) findViewById(R.id.button2);
+        final EditText editText = (EditText) findViewById(R.id.editText);
+
+        SlidingAnimationListener listener = new SlidingAnimationListener();
+        translateBottom.setAnimationListener(listener);
+        translateClose.setAnimationListener(listener);
+
+
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(isPageOpen){
+                    slide.startAnimation(translateClose);
+                }
+                else{
+                    slide.setVisibility(View.VISIBLE);
+                    slide.startAnimation(translateBottom);
+                }
+
+            }
+        });
+
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                slide.startAnimation(translateClose);
+                webView.loadUrl(editText.getText() + "");
+            }
+        });
+
+
+    }
+
+    class SlidingAnimationListener implements Animation.AnimationListener {
+        @Override
+        public void onAnimationStart(Animation animation) {
+
+        }
+
+        @Override
+        public void onAnimationEnd(Animation animation) {
+            if(isPageOpen){
+                slide.setVisibility(View.INVISIBLE);
+                button.setText("열기");
+                isPageOpen = false;
+            }else{
+                button.setText("닫기");
+                isPageOpen = true;
+            }
+        }
+
+        @Override
+        public void onAnimationRepeat(Animation animation) {
+
+        }
     }
 }
---
+++ C8WebView/app/src/main/java/org/androidtown/c8webview/MainFragment.java
@@ -0,0 +1,24 @@
+package org.androidtown.c8webview;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by young on 2018-03-30.
+ */
+
+public class MainFragment extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        ViewGroup rootView = (ViewGroup) inflater.inflate(R.layout.url_input, container, false);
+
+        return rootView;
+
+    }
+}
