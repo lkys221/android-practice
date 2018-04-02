036c895eb5bb7e5c68c64a8186c33cedf285e179
Leo .Kim <lkys221@snu.ac.kr>
2018-04-02 20:50:48 +0900

    C10 init

---
+++ C10ShoppingGrid/app/src/main/java/org/androidtown/c10_shoppinggrid/MainActivity.java
@@ -0,0 +1,13 @@
+package org.androidtown.c10_shoppinggrid;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
