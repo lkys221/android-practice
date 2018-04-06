4317fcc3ea3fc727899db6d2b01a283426ee346c
Leo .Kim <lkys221@snu.ac.kr>
2018-04-06 21:08:37 +0900

    C10 complete

--- C10ShoppingGrid/app/src/main/java/org/androidtown/c10_shoppinggrid/MainActivity.java
+++ C10ShoppingGrid/app/src/main/java/org/androidtown/c10_shoppinggrid/MainActivity.java
@@ -4,16 +4,43 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.BaseAdapter;
+import android.widget.EditText;
+import android.widget.GridView;
+import android.widget.ListView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+    EditText editText;
+    GridView gridView;
+    ShoppingAdapter adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        gridView = (GridView) findViewById(R.id.gridView);
+
+        adapter = new ShoppingAdapter();
+        adapter.addItem(new ShoppingItem("Dog1", "1000", "Happy dog", R.drawable.dog1));
+        adapter.addItem(new ShoppingItem("Dog2", "2000", "Sad dog", R.drawable.dog2));
+        adapter.addItem(new ShoppingItem("Dog3", "3000", "Cute dog", R.drawable.dog3));
+        adapter.addItem(new ShoppingItem("Dog4", "4000", "Little dog", R.drawable.dog4));
+
+        gridView.setAdapter(adapter);
+
+        gridView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                ShoppingItem item = (ShoppingItem) adapter.getItem(position);
+                Toast.makeText(getApplicationContext(), "선택: " + item.getName(), Toast.LENGTH_LONG).show();
+            }
+        });
+
     }
 
     class ShoppingAdapter extends BaseAdapter{
