49f6160cdb1b52192972a870b25a4a080184d59e
Leo .Kim <lkys221@snu.ac.kr>
2018-04-08 23:31:14 +0900

    rec center

---
+++ C12RedRectangle/app/src/main/java/org/androidtown/c12_redrectangle/Rectangle.java
@@ -0,0 +1,54 @@
+package org.androidtown.c12_redrectangle;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Point;
+import android.graphics.Rect;
+import android.support.annotation.Nullable;
+import android.util.AttributeSet;
+import android.view.View;
+
+/**
+ * Created by young on 2018-04-08.
+ */
+
+public class Rectangle extends View {
+    Paint paint;
+
+    public Rectangle(Context context) {
+        super(context);
+
+        init(context);
+    }
+
+    public Rectangle(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+
+        init(context);
+    }
+
+    public void init(Context context){
+        paint = new Paint();
+        paint.setColor(Color.RED);
+
+    }
+
+    @Override
+    protected  void onDraw(Canvas canvas){
+        super.onDraw(canvas);
+
+        int canvasW = getWidth();
+        int canvasH = getHeight();
+        Point centerOfCanvas = new Point(canvasW / 2, canvasH / 2);
+        int rectW = 100;
+        int rectH = 100;
+        int left = centerOfCanvas.x - (rectW / 2);
+        int top = centerOfCanvas.y - (rectH / 2);
+        int right = centerOfCanvas.x + (rectW / 2);
+        int bottom = centerOfCanvas.y + (rectH / 2);
+        Rect rect = new Rect(left, top, right, bottom);
+        canvas.drawRect(rect, new Paint());
+    }
+}
